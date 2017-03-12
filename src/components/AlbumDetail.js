4d536158406fe799b6e2c4834a2c87f2a03c5d6b
yijutseng <beergt@gmail.com>
2017-03-12 22:29:32 +0800

    ch53

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import {Text,View,Image} from 'react-native';
+import {Text,View,Image,Linking} from 'react-native';
 import Card from './Card';
+import Button from './Button';
 import CardSection from './CardSection';
 
 //import axios from 'axios';
 
 const AlbumDetail=({album}) =>{
-	const {title, artist,thumbnail_image}=album;
-	const {thumbnailStyle,headerContentStyle}=styles;
+	const {title, artist,thumbnail_image,image,url}=album;
+	const {
+		thumbnailStyle,
+		headerContentStyle,
+		thumbnailContainerStyle,
+		headerTextStyle,
+		imageStyle
+		}=styles;
 	return (
 		<Card>
 			<CardSection>
-				<View>
+				<View style= {thumbnailContainerStyle}>
 					<Image
 						style={thumbnailStyle}
 						source={{uri: thumbnail_image}}
 						/>
 				</View>
 				<View style={headerContentStyle}>
-					<Text>{title}</Text> 
+					<Text style= {headerTextStyle}>{title}</Text> 
 					<Text>{artist}</Text> 
 				</View>
+			</CardSection>
+			
+			<CardSection>
+				<Image
+					style={imageStyle}
+					source={{uri: image}}
+					/>
+			</CardSection>
+			
+			<CardSection>
+				<Button onPress={()=>Linking.openURL(url)}>
+					<Text> Buy {title} Now!</Text> 
+				</Button>
 			</CardSection>
 		</Card>
 	);
@@ -31,9 +51,23 @@ const styles= {
 		backgroundColor:'#fff',
 		justifyContent:'space-around',
 	},
+	headerTextStyle:{
+		fontSize:18
+	},
 	thumbnailStyle:{
 		height:50,
 		width:50
+	},
+	thumbnailContainerStyle:{
+		justifyContent:'center',
+		alignItems:'center',
+		marginLeft: 10,
+		marginRight:10
+	},
+	imageStyle:{
+		height:300,
+		flex:1,
+		width:null
 	}
 };
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 //import axios from 'axios';
@@ -28,9 +28,9 @@ class AlbumList extends Component{
         console.log(this.state)
         //Life cycle method
         return(
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
